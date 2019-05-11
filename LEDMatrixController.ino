8065524c4bd79616be9025c51991e3c68be91dc9
Darrenhal <Nicolaneher@kabelmail.de>
2019-05-12 01:20:21 +0200

    implemented int[14][14][3] read logic

--- LEDMatrixController.ino
+++ LEDMatrixController.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 /*Definieren der Output-Pins für die ersten 7 LED Streifen*/
@@ -32,22 +32,40 @@ void setup() {
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif
 
-/*Seriellen Port über den der Pi sich mit dem Arduino verbindet einrichten*/
-  Serial.begin(9600);
+  /*Seriellen Port über den der Pi sich mit dem Arduino verbindet einrichten*/
+  Serial.begin(115200); //setzen der Bitrate auf 115200 Bit pro Sekunde
   Serial.setTimeout(50);
 
-/*NeoPixel Library initialisieren*/
+  /*NeoPixel Library initialisieren*/
   pixels.begin();
 }
 
 //infinite loop refreshing the matrix
 void loop() {
   /*dreidimensionales int Array einlesen
-  erste Dimension enthält Adresse des adressierten LED Streifens
-  zweite Dimension enthält Adresse des adressierten Pixels im Streifen
-  dritte Dimension enthält RGB Wert für adressiertes Pixel
+    erste Dimension enthält Adresse des adressierten LED Streifens
+    zweite Dimension enthält Adresse des adressierten Pixels im Streifen
+    dritte Dimension enthält RGB Wert für adressiertes Pixel
   */
-  int[][][] = Serial.readInt[][][]();
+  int matrix[14][14][3];
+  matrix = Serial.read();
+
+  int bytesRead = 0;
+  int i = 0;
+  int j = 0;
+
+  while (bytesRead < 2352) {
+    if (Serial.available >= 12) {
+      for (i < 14; i++) {
+        for (j < 14; i++) {
+          matrix[i][j][0] = Serial.parseInt();
+          matrix[i][j][1] = Serial.parseInt();
+          matrix[i][j][2] = Serial.parseInt();
+          bytesRead += 12;
+        }
+      }
+    }
+  }
 
   //Iterieren über alle Pixel pro Reihe und leeren dieser, um einen globalen init Zustand zu erhalten
   for (int i = 0; i < NUMSTRIPS; i++) {
@@ -55,20 +73,20 @@ void loop() {
   }
 
   /*Iterieren über Anzahl der Pixels pro Reihe und setzen der RGB Werte
-  Danach erneutes iterieren, um die neuen Pixel-Farben anzuzeigen*/
-  for(int i=0;i<NUMPIXELS;i++){
+    Danach erneutes iterieren, um die neuen Pixel-Farben anzuzeigen*/
+  for (int i = 0; i < NUMPIXELS; i++) {
 
-    row[0].setPixelColor(i, row[0].Color(255, i*5, 0));
-    row[1].setPixelColor(i, row[1].Color(0, i*5, 255));
-    row[2].setPixelColor(i, row[2].Color(255, i*5, 255));
-    row[3].setPixelColor(i, row[3].Color(i*5, i*5, 255));
-    row[4].setPixelColor(i, row[4].Color(255, i*5, i*5));
-    row[5].setPixelColor(i, row[5].Color(0, i*5, 0));
-    row[6].setPixelColor(i, row[6].Color(0, 0, i*5));
+    row[0].setPixelColor(i, row[0].Color(255, i * 5, 0));
+    row[1].setPixelColor(i, row[1].Color(0, i * 5, 255));
+    row[2].setPixelColor(i, row[2].Color(255, i * 5, 255));
+    row[3].setPixelColor(i, row[3].Color(i * 5, i * 5, 255));
+    row[4].setPixelColor(i, row[4].Color(255, i * 5, i * 5));
+    row[5].setPixelColor(i, row[5].Color(0, i * 5, 0));
+    row[6].setPixelColor(i, row[6].Color(0, 0, i * 5));
 
-    for(int i = 0; i < NUMSTRIPS; i++) {
+    for (int i = 0; i < NUMSTRIPS; i++) {
       row[i].show();
     }
 
     delay(DELAY); //10ms warten bevor loop() erneut durchlaufen wird
-}
+  }
